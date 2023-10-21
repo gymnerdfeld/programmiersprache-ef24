ec4548ef2a69d77e8b555cfc42ee79eb8ca44bbd
Marco Schmalz <marco@schess.ch>
2023-10-21 12:17:02 +0200

    programmiersprache.py: clean ups

--- programmiersprache.py
+++ programmiersprache.py
@@ -1,7 +1,9 @@
+# ====================================== #
+# Die ersten beiden Phasen der Auführung #
+# ====================================== #
 def tokenize(program):
     """Programmtext in die logischen Blöcke aufteilen, und Kommentare entfernen
     """
-    
     # Kommentare entfernen
     zeilen_alt = program.split("\n")
     zeilen_neu = []
@@ -43,6 +45,9 @@ def parse_atom(token):
     else:
         return token
 
+# ===================== #
+# Eingebaute Funktionen #
+# ===================== #
 def add(a, b):
     return a + b
 
@@ -61,12 +66,14 @@ def div_int(a, b):
 def expt(a, b):
     return a ** b
 
+def less_than(a, b):
+    return a < b
+
 def block(*args):
     if len(args) == 0:
         return None
     else:
         return args[-1]
-
 
 def dict_new():
     return {}
@@ -83,62 +90,142 @@ def dict_get(d, key):
 
 import math
 import random
-import operator as op
 
-# Eingebaute Funktionen
+# Dict mit den eingebaute Funktionen
 builtins = {
+    # Die wichtigsten mathematischen Operationen
     "+": add,
     "-": sub,
     "*": mult,
     "/": div,
     "//": div_int,
-    "expt": expt,
+    "expt": expt,   # "hoch"
     "sin": math.sin,
-    "<": op.lt,
     "random": random.random,
+
+    # Vergleichsoperator
+    "<": less_than,
+
+    # Mehrere Ausdrücke nacheinander ausführen
     "block": block,
+
+    # Pythons True und False
     "True": True,
     "False": False,
+
+    # Support für dicts
     "dict-new": dict_new,
     "dict-in": dict_in,
     "dict-add": dict_add,
     "dict-get": dict_get,
 }
 
-# Library (in unserer Sprache geschriebener Code)
+# =============================================== #
+# Library (in unserer Sprache geschriebener Code) #
+# =============================================== #
 library = """
 (block
-    ;;;;;;;;;;;; Mathematische Funktionen ;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;; Weitere Vergleichsfunktionen ;;;;;;;;;;;;;;;;;;
+    (sto > (phonk (a b) (< b a)))
+    (sto >= (phonk (a b) (if (< a b) False True)))
+    (sto not (phonk (a) (if a False True)))
+    (sto <= (phonk (a b) (not (> a b)) ))
+
+    ;;;;;;;;;;;; Mathematische Konstanten ;;;;;;;;;;;;;;;;;;
     (sto pi 3.141592653589793)
     (sto e 2.718281828459045)    ; Eulersche Zahl
-    (sto cos (phonk (a) (sin (+ a (/ pi 2)))))
-    (sto fact (phonk (n)
-        (if (< n 2) 1 (* n (fact (- n 1))))
+
+    ;;;;;;;;;;;; Mathematische Funktionen ;;;;;;;;;;;;;;;;;;
+    ; Cosinus
+    (sto cos (phonk (x)
+        (sin (+ x (/ pi 2)))
     ))
+
+    ; Fakultät
+    (sto fact (phonk (n)
+        (if (< n 2)
+            1
+            (* n (fact (- n 1)))
+        )
+    ))
+
+    ; Betrag
     (sto abs (phonk (x)
         (if (< x 0)
             (- 0 x)
             x
         )
     ))
-    
-    ;;;;;;;;;;;; Vergleichsfunktionen ;;;;;;;;;;;;;;;;;;
-    
-    (sto > (phonk (a b) (< b a)))
-    (sto >= (phonk (a b) (if (< a b) False True)))
-    
-    (sto not (phonk (a) (if a False True)))
+
+    ; Fibonacci-Zahlen, rekursiv (langsam) implementiert
+    (sto fib (phonk (n)
+        (if (< n 2)
+            n
+            (+ (fib (- n 1)) (fib (- n 2))))
+    ))
+
+    ; Quadratwurzel
+    (sto sqrt (phonk (x) (block
+        (sto sqrt-iter (phonk (guess)
+            (if (good-enough? guess)
+                guess
+                (sqrt-iter (improve guess))
+            )
+        ))
+        (sto good-enough? (phonk (guess)
+            (< (abs (- (* guess guess) x)) 0.001)
+        ))
+        (sto improve (phonk (guess)
+            (average guess (/ x guess))
+        ))
+        (sto average (phonk (x y)
+            (/ (+ x y) 2)
+        ))
+        (sqrt-iter 1)
+    )))
+
+    ;;;;;;;;;;;; Cached "decorator" ;;;;;;;;;;;;;;;;;;
+    (sto cached (phonk (func) (block
+        (sto cache (dict-new))
+        (sto new_func (phonk (x)
+            (if (dict-in cache x)
+                (dict-get cache x)
+                (block
+                    (sto res (func x))
+                    (dict-add cache x res)
+                    res
+                )
+        )))
+        new_func
+    )))
+
+    ; Cached "decorator" auf fib anwenden
+    (sto fib (cached fib))
+
+    ;;;;;;;;;;;; Closure Experimente ;;;;;;;;;;;;;;;;;;
+    (sto make_adder (phonk (x) (block
+        (sto adder (phonk (y)
+            (+ x y)
+        ))
+        adder
+    )))
+
+    (sto plus5 (make_adder 5))
 )
 """
 
 # Globales Environment
 global_env = [{}, builtins]
 
+# =============================================== #
+# Dritte Phase der Ausführung: Audrücke auswerten #
+# =============================================== #
 def evaluate(expr, env=global_env):
     match expr:
         # Einfache Werte
         case int(number) | float(number):
             return number
+        # Variablen
         case str(name):
             # Variablen im Environment von vorne nach hinten
             # suchen
@@ -149,8 +236,11 @@ def evaluate(expr, env=global_env):
             # Nichts gefunden unter dem angegebenen Name: Error
             raise NameError(f"name '{name}' is not defined")
 
-        # Spezialkonstrukte
-        case ["sto", name, value]:     # Wert abspeichern
+        #####################
+        # Spezialkonstrukte #
+        #####################
+        # Wert abspeichern
+        case ["sto", name, value]:
             value = evaluate(value, env)
 
             # Neue Variable im vordersten dict vom Environment
@@ -158,20 +248,25 @@ def evaluate(expr, env=global_env):
             local_env = env[0]
             local_env[name] = value
             return f"{name} stored: {value}"
-        case ["phonk", [*params], body]:  # Funktionsdefinition
-            return ["phonk", params, body]
+        # Funktion definieren
+        case ["phonk", [*params], body]:
+            return ["phonk", params, body, env]
+        # If
         case ["if", condition, true_body, false_body]:
             if evaluate(condition, env):
                 return evaluate(true_body, env)
             else:
                 return evaluate(false_body, env)
 
-        # Funktionen aufrufen
+        #######################
+        # Funktionen aufrufen #
+        #######################
         case [operator, *args]:
             function = evaluate(operator, env)
             args = [evaluate(arg, env) for arg in args]
             match function:
-                case ["phonk", params, body]:   # Eigene Funktionen
+                # "Eigene" Funktionen in unserer Sprache
+                case ["phonk", params, body, closure_env]:
                     # Neues dict für lokale Variablen erstellen
                     local_env = {}
 
@@ -184,18 +279,23 @@ def evaluate(expr, env=global_env):
 
                     # Neues Environment erstellen, mit den neuen
                     # lokalen Variablen zuvorderst
-                    new_env = [local_env, *env]
+                    new_env = [local_env, *closure_env]
 
                     # Body in neuem Environment ausführen / evaluieren
                     return evaluate(body, new_env)
-                case _:                         # Eingebaute Funktionen (in Python geschrieben)
+
+                # Eingebaute Funktionen (in Python geschrieben)
+                case _:
                     return function(*args)
 
         # Unbekannter Ausdruck -> Fehler
         case _:
             raise ValueError("Unbekannter Ausdruck")
 
-#=====================
+# =================================== #
+# Hilfscode für die Konsole und Tests #
+# =================================== #
+
 def run(program):
     return evaluate(parse(tokenize(program)))
 
